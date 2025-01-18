e768ee0964241fdb090b063657b5143efbaaf713
panjx-7339 <panjinxia06@gmail.com>
2025-01-18 22:05:13 +0800

    Add name state and update button

--- my-react-app/src/components/CollabPage/SongManagerBar.jsx
+++ my-react-app/src/components/CollabPage/SongManagerBar.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
 import "./SongManagerBar.css";
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { IoFilterOutline as FilterIcon} from "react-icons/io5";
-import { MdAddCircleOutline as AddIcon } from "react-icons/md";
-import { AiOutlineMinusCircle as DeleteIcon } from "react-icons/ai";
 
 
 
 function SongManagerBar() {
+  const [inputName, setInputName] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();  
+    props.setUser(inputName)
+    console.log("Input Name: " + inputName)
+  }
+
   return (
     <div className="manager-bar">
       <Form className="d-flex align-items-center ms-3 col-4"> 
@@ -17,13 +23,13 @@ function SongManagerBar() {
           id="name" 
           name="name" 
           required 
+          onChange={(e) => setInputName(e.target.value)}
           className="flex-grow-1 col-xs-1 ms-1 me-2 form-control-sm"
           style={{ minWidth: '80px' }}></Form.Control>
-        <Button type="submit" className="bg-black border-0" style={{fontSize: '14px'}}>Submit</Button>
+        <Button onClick={handleSubmit} className="bg-black border-0" style={{fontSize: '14px'}}>Submit</Button>
       </Form>
       <div className="d-flex justify-content-end me-5">
-        <Button variant="outline-dark"><FilterIcon /></Button>
-        <Button variant="outline-dark"><DeleteIcon /></Button>
+        <Button variant="outline-dark border-0"><FilterIcon /></Button>
       </div>   
     </div>
   )
