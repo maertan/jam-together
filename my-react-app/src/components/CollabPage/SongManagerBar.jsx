c3090263eef7cd5096c95d9023bac1f7a2fb73e0
panjx-7339 <panjinxia06@gmail.com>
2025-01-18 21:44:49 +0800

    Change to Bootstrap Styling

--- my-react-app/src/components/CollabPage/SongManagerBar.jsx
+++ my-react-app/src/components/CollabPage/SongManagerBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./SongManagerBar.css";
+import { Form, Button, Row, Col } from 'react-bootstrap';
 import { IoFilterOutline as FilterIcon} from "react-icons/io5";
 import { MdAddCircleOutline as AddIcon } from "react-icons/md";
 import { AiOutlineMinusCircle as DeleteIcon } from "react-icons/ai";
@@ -7,20 +8,24 @@ import { AiOutlineMinusCircle as DeleteIcon } from "react-icons/ai";
 
 
 function SongManagerBar() {
-  // add action attribute for form
   return (
-    <div class="manager-bar">
-      <form> 
-        <label for="name">Enter Your Name: </label>
-        <input type="text" id="name" name="name" required></input>
-        <button type="submit">Submit</button>
-      </form>
-      <div class="edit-bar">
-        <button type="edit"><FilterIcon /></button>
-        <button type="edit"><AddIcon /></button>
-        <button type="edit"><DeleteIcon /></button>
-      </div>
-      
+    <div className="manager-bar">
+      <Form className="d-flex align-items-center ms-3 col-4"> 
+        <Form.Label className="col-3 mb-0" style={{ fontSize: '14px' }}>Enter Your Name: </Form.Label>
+        <Form.Control 
+          type="text" 
+          id="name" 
+          name="name" 
+          required 
+          className="flex-grow-1 col-xs-1 ms-1 me-2 form-control-sm"
+          style={{ minWidth: '80px' }}></Form.Control>
+        <Button type="submit" className="bg-black border-0" style={{fontSize: '14px'}}>Submit</Button>
+      </Form>
+      <div className="d-flex justify-content-end me-5">
+        <Button variant="outline-dark"><FilterIcon /></Button>
+        <Button variant="outline-dark"><AddIcon /></Button>
+        <Button variant="outline-dark"><DeleteIcon /></Button>
+      </div>   
     </div>
   )
 
