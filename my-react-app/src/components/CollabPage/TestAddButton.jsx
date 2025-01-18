69efdb50c128a346de7e35e5c0df90d00740b564
panjx-7339 <panjinxia06@gmail.com>
2025-01-19 04:30:56 +0800

    Add song adding functionality

---
+++ my-react-app/src/components/CollabPage/TestAddButton.jsx
@@ -0,0 +1,26 @@
+// // Test Component for adding songs
+// import React from 'react';
+// import { addSong } from '../../logic/addSong';
+
+// function TestAddButton() {
+//   const handleTest = async () => {
+//     const data = {
+//       artist: "SEVENTEEN",
+//       title: "God of Moooosic",
+//       duration_ms: 205000,
+//       spotify_url: "https://open.spotify.com/track/4MTNimMDRyFZv37Thcktwa?si=9c6d9c6da4d84099",
+//       collab_id: "SS8l3K9", 
+//       song_id: "4MTNimMDRyFZv37Thcktwa",
+//       image_url: "https://i.scdn.co/image/ab67616d0000b273fd8d7a8d96871e791cb1f626",
+//     }
+//     await addSong(data);
+//   };
+
+//   return (
+//     <div>
+//       <button onClick={handleTest}>Test Add Song</button>
+//     </div>
+//   );
+// }
+
+// export default TestAddButton;
