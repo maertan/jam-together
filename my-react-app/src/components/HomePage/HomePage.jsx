22ebd2bd45d26cf0bfa7ef69b5f8e647be0ac842
panjx-7339 <panjinxia06@gmail.com>
2025-01-19 12:04:16 +0800

    Fix JoinCreate alignment

--- my-react-app/src/components/HomePage/HomePage.jsx
+++ my-react-app/src/components/HomePage/HomePage.jsx
@@ -6,10 +6,10 @@ import AboutBox from './AboutBox';
 const HomePage = () => {
   return (
     <>
-      <Container  className="p-4 mb-4 w-75 justify-content-center align-items-center"> 
+      <Container className="p-4 mb-4 w-75 justify-content-center align-items-center"> 
         <AboutBox />
       </Container>
-      <Container fluid="true" className="p-4 w-100"> 
+      <Container fluid="true" className="w-75 mt-0 m-auto justify-content-center align-items-center"> 
         <JoinCreate />
       </Container>
     </>
