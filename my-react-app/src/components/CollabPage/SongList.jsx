d2689cd5e1d95669662c1e238ced3348b6c4cdce
dovlin <dovlincs01@gmail.com>
2025-01-19 09:20:54 +0800

    Shift some logic to CollabPage

--- my-react-app/src/components/CollabPage/SongList.jsx
+++ my-react-app/src/components/CollabPage/SongList.jsx
@@ -5,44 +5,18 @@ import SongCard from './SongCard.jsx';
 import { loadSongList } from '../../logic/loadSongList.jsx';
 
 const SongList = (props) => {
-    const [error, setError] = useState("");
-    const [songCards, setSongCards] = useState([
-    ]);
-
     console.log("Songlist - Final collab_id:", props.collab_id); // Debug final collab_id XHlvyF6 (L NOT I)
-
-    const handleSongLoading = async () => {
-        try {
-            
-            const songList = await loadSongList(props.collab_id)
-            if (songList) {
-                setSongCards(songList);
-            }
-        } catch (error) {
-            setError(error)
-        }
-    }
-
-    useEffect(() => {
-        handleSongLoading();
-    }, [props.collab_id, props.user])
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-
 
     return (
         <div>
             {
-                songCards.map((song) => (
+                props.songCards.map((song) => (
                     <SongCard 
                         user = {props.user}
                         songInfo={{id:song.id, title: song.title, artist: song.artist,image_url: song.image_url, votes:song.votes, owner: song.owner}}
                         upvotes= {song.upvotes}
                         downvotes = {song.downvotes}
-                        handleSongLoading={handleSongLoading} 
+                        handleSongLoading={props.handleSongLoading} 
                     />
 
                 ))
